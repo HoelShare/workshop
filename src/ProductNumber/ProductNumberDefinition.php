6e055567279050191d2651e2b7e166843e53395c
Sebastian Hölscher <s.hoelscher@shopware.com>
2020-12-15 08:04:54 +0100

    Add ReferenceVersionField

--- src/ProductNumber/ProductNumberDefinition.php
+++ src/ProductNumber/ProductNumberDefinition.php
@@ -9,6 +9,7 @@ use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ReferenceVersionField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\TranslatedField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\TranslationsAssociationField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
@@ -27,6 +28,7 @@ class ProductNumberDefinition extends EntityDefinition
         return new FieldCollection([
             (new IdField('id', 'id'))->addFlags(new PrimaryKey(), new Required()),
             (new FkField('product_id', 'productId', ProductDefinition::class))->addFlags(new Required()),
+            (new ReferenceVersionField(ProductDefinition::class))->addFlags(new Required()),
             (new TranslatedField('number'))->addFlags(new Required()),
             (new TranslationsAssociationField(ProductNumberTranslationDefinition::class, 'product_number_id')),
             (new ManyToOneAssociationField('product', 'product_id', ProductDefinition::class)),
